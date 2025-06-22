7c129fc0ee28aed1e551e24c412d53918ffc7602
Muhammad Reivan N M <reivnnaufl@gmail.com>
2025-06-22 14:34:56 +0700

    change share label

--- server/app/service/firestore.py
+++ server/app/service/firestore.py
@@ -16,11 +16,10 @@ if not cred_path:
 db = firestore.Client.from_service_account_json(current_dir.parent.parent / cred_path)
 
 def save_share_data(share1_b64: str, share2_b64: str, name: str, width: int, marker_size: int, userid: str) -> dict:
-    print("cek nama: " + name)
     # Prepare the data structure
     qr_data = {
-        "share1": share1_b64,
-        "share2": share2_b64,
+        "public_share": share1_b64,
+        "private_share": share2_b64,
         "metadata": {
             "original_width": width,
             "marker_size": marker_size,
